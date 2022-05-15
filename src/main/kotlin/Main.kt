ee8a9fb35b1ee0d15ba05a7ffa7259ab405128ad
Евгений <eugeny1988@gmail.com>
2022-05-15 16:28:59 +0300

    Changed conditional

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,34 +7,41 @@ val saleToRegularCustomer = 0.01
 fun main() {
     println("Введите сумму покупки: ")
     var sum = readln().toDouble()
-    val scoreSale = if (isCustomerRegular && sum < firstSum) sum * saleToRegularCustomer
-    else if (isCustomerRegular && (sum >= firstSum) && (sum < secondSum)) (sum - firstSale) * saleToRegularCustomer
-    else if (isCustomerRegular && (sum >= secondSum)) sum * saleToRegularCustomer * (1 - secondSale) + sum * secondSale
-    else if (!isCustomerRegular && sum < firstSum) sum
-    else if (!isCustomerRegular && (sum >= firstSum) && (sum < secondSum)) firstSale
-    else if (!isCustomerRegular && (sum >= secondSum)) sum * secondSale
-    else return
+    val scoreSale =
+        if (isCustomerRegular) {
+            if (sum < firstSum) {
+                sum * saleToRegularCustomer
+            }
+            else if ((sum >= firstSum) && (sum < secondSum)) (sum - firstSale) * saleToRegularCustomer
+            else if ((sum >= secondSum)) sum * saleToRegularCustomer * (1 - secondSale) + sum * secondSale
+            else return
+        } else {
+            if (sum < firstSum) sum
+            else if ((sum >= firstSum) && (sum < secondSum)) firstSale
+            else if ((sum >= secondSum)) sum * secondSale
+            else return
+        }
     val score = sum - scoreSale
-    val sumRoubles = sum.toInt()
-    val sumKopecks = Math.round(sum - sumRoubles)
-    val saleRoubles = score.toInt()
-    val saleKopecks = Math.round(score - saleRoubles)
-    println("Покупка: $sumRoubles руб. $sumKopecks коп.")
-    if (isCustomerRegular && sum < firstSum) {
-        println("После применения 1-% скидки: $saleRoubles руб. $saleKopecks коп.")
+    println("Покупка: $sum")
+    if (isCustomerRegular) {
+        if (sum < firstSum) {
+            println("После применения скидки 1%: $scoreSale")
+        }
+        if ((sum >= firstSum) && (sum < secondSum)) {
+            println("После применения скидки 100 рублей: ${sum - secondSale}")
+            println("После применения 1-% скидки: $scoreSale")
+        }
+        if (sum >= secondSum) {
+            println("После применения 5-% скидки: ${sum*secondSale}")
+            println("После применения 1-% скидки: $scoreSale")
+        }
     }
-    if (isCustomerRegular && (sum >= firstSum) && (sum < secondSum)) {
-        println("После применения скидки 100 рублей: ${(sum - 100).toInt()} руб. ${Math.round((sum - 100 - (sum - 100).toInt()) * 100)} коп.")
-        println("После применения 1-% скидки: $saleRoubles руб. ${saleKopecks} коп.\"")
-    }
-    if (isCustomerRegular && (sum >= secondSum)) {
-        println("После применения 5-% скидки: ${(sum * 0.05).toInt()} руб. ${Math.round((sum * 0.05 - (sum * 0.05).toInt()) * 100)} коп.")
-        println("После применения 1-% скидки: $saleRoubles руб. ${saleKopecks} коп.\"")
-    }
-    if (!isCustomerRegular && sum < firstSum)
+    if (!isCustomerRegular){
+         if (sum < firstSum)
         println("У Вас нет никаких скидок")
-    if (!isCustomerRegular && (sum >= firstSum) && (sum < secondSum))
-        println("После применения скидки 100 рублей: $saleRoubles руб. ${saleKopecks} коп.\"")
-    if (!isCustomerRegular && (sum >= secondSum))
-        println("После применения 5-% скидки: $saleRoubles руб. ${saleKopecks} коп.\"")
+    if ((sum >= firstSum) && (sum < secondSum))
+        println("После применения скидки 100 рублей: $scoreSale")
+    if (sum >= secondSum)
+        println("После применения 5-% скидки: $scoreSale")
+    }
 }
